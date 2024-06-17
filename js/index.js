2a0bcf812d42eb28684987b5103494b4d7c827b0
jy <hmou011777@gmail.com>
2024-06-17 10:47:31 +0900

    poto

--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
   $(window).on("scroll", function () {
     let sc = $(window).scrollTop();
 
+    // 이미지 크기 조정 조건
+    let panelWidth = $(window).width() <= 350 ? 350 : 1200;
+    let panelHeight = $(window).width() <= 350 ? "250%" : "180%";
+
     if (sc >= 900 && sc <= 1350) {
-      $(".bg").stop().animate({ width: "200%", height: "250%" });
+      $(".bg").stop().animate({ width: panelWidth, height: panelHeight });
     }
 
     let baseline = -200;
@@ -44,14 +48,12 @@ $(document).ready(function () {
     $("#navi li").removeClass("on");
     $("#navi li").eq(navi).addClass("on");
 
-    let move = $("#conatainer").eq(navi).offset();
+    let move = $("#container").eq(navi).offset();
     $("html, body").stop().animate({ scrollTop: move });
   });
 
-  // let total = $(".panel li").length;
-  let total = 4;
+  let total = $(".panel li").length;
   let i = 0;
-  console.log(total);
 
   $(".fa-circle-chevron-right").on("click", function () {
     if (i == total - 1) {
@@ -59,8 +61,8 @@ $(document).ready(function () {
     } else {
       i++;
     }
-    let newright = -500 * i;
-    $(".panel").animate({ "margin-left": newright + "px" });
+    let newRight = -panelWidth * i; // 이미지의 너비에 따라 조정
+    $(".panel").animate({ "margin-left": newRight + "px" });
   });
 
   $(".fa-circle-chevron-left").on("click", function () {
@@ -69,29 +71,21 @@ $(document).ready(function () {
     } else {
       i--;
     }
-    let newLeft = 500 * i; // 이미지의 너비에 따라 조정
+    let newLeft = panelWidth * i; // 이미지의 너비에 따라 조정
     $(".panel").animate({ "margin-left": -newLeft + "px" });
   });
 
-  // $(".graphic1").click(function () {
-  //   $(this).find(".modal").css("display", "block");
-  // });
-
-  // $(".close").click(function () {
-  //   $(".modal").css({ display: "none" });
-  // });
   $(document).ready(function () {
-    // 페이지 로드시 팝업 overlay를 표시합니다.
     $(".popup-overlay").fadeIn();
 
     $(".popup-overlay, .close").click(function () {
       $(".popup, .popup-overlay").fadeOut();
-      $("body").removeClass("modal-open"); // 모달 닫을 때 클래스 제거
+      $("body").removeClass("modal-open");
     });
 
     $(".popup").click(function (event) {
       event.stopPropagation();
-    }); /*못나오게 하기*/
+    });
 
     $(".graphic1").click(function () {
       $("#graphicPopup, .popup-overlay").fadeIn();
@@ -114,29 +108,10 @@ $(document).ready(function () {
       $("body").addClass("modal-open");
     });
   });
-});
-$(".popup-overlay").fadeIn();
 
-$(".popup-overlay, .close").click(function () {
-  $(".popup, .popup-overlay").fadeOut();
-});
-
-$(".popup").click(function (event) {
-  event.stopPropagation();
-});
-$(function () {
-  $(".marquee").marquee({
-    duration: 15000,
+  $(function () {
+    $(".marquee").marquee({
+      duration: 15000,
+    });
   });
 });
-// $(".fa-circle-chevron-right").on("click", function () {
-//   let i = parseInt($(".panel").css("margin-left")) || 0;
-//   let next = i - 550;
-//   let max = -550 * ($(".panel li").length - 1);
-//   if (next < max) {
-//     i = 0;
-//   } else {
-//     i = next;
-//   }
-//   $(".panel").animate({ "margin-left": i + "px" });
-// });
